31396780063c5fabcb51b00875d69554135ac063
Mark White <67396605+Marko141stack@users.noreply.github.com>
2022-03-01 09:12:51 +0300

    Create comingsoon.js

---
+++ coming soon/comingsoon.js
@@ -0,0 +1,17 @@
+
+$(function(){
+
+  
+  $('#btn-ok').on('click',function(){
+   
+    if($('#emailadd').val().length === 0){
+      alert('Please enter your email to subscribe.');
+    }else{
+       $(".input-group-lg").css('color', '#f90').html("Thanks for the subscription.");
+      $("#myModal").modal("show");
+    }
+    
+    
+  });
+  
+});
